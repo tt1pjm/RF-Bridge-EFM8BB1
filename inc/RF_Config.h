1eaf2c52145ea49aa503b0390127125440f90d9b
tt1pjm <48781860+tt1pjm@users.noreply.github.com>
2019-03-22 21:40:13 +0100

    Revert "5514 SILENT Dual Tech support"
    This reverts commit a03ae1b8d300cee4589be01960e54a028c037922.

--- inc/RF_Config.h
+++ inc/RF_Config.h
@@ -21,7 +21,7 @@
 #define EFM8BB1_SUPPORT_Rohrmotor24_PROTOCOL	0		// Rohrmotor24
 #define EFM8BB1_SUPPORT_PAR56_PROTOCOL			0		// UNDERWATER PAR56 LED LAMP, 502266
 #define EFM8BB1_SUPPORT_WS_1200_PROTOCOL		0		// Alecto WS-1200 Series Wireless Weather Station
-#define EFM8BB1_SUPPORT_ALDI_4x_PROTOCOL		0		// ALDI Remote controlled wall sockets, 4x
+#define EFM8BB1_SUPPORT_ALDI_4x_PROTOCOL		1		// ALDI Remote controlled wall sockets, 4x
 #define EFM8BB1_SUPPORT_SP45_PROTOCOL			1		// Meteo SPxx -  Weather station (PHU Metrex)
 #define EFM8BB1_SUPPORT_DC90_PROTOCOL			1		// Dooya DC90 remote
 #define EFM8BB1_SUPPORT_DG_HOSA_PROTOCOL		1		// Digoo DG-HOSA Smart 433MHz Wireless Household Carbon Monoxide Sensor
@@ -33,4 +33,3 @@
 #define EFM8BB1_SUPPORT_Prologue_PROTOCOL		1		// Prologue Sensor, Issue #96
 #define EFM8BB1_SUPPORT_DOG_COLLAR_PROTOCOL		1		// Generic dog training collar - board label T-187-n (TX)-1, PR #100
 #define EFM8BB1_SUPPORT_BY302_PROTOCOL			1		// Byron BY302 Doorbell, Issue #102
-#define EFM8BB1_SUPPORT_DT_5514_PROTOCOL        1       // 5514 SILENT Dual Tech, Issue #104
